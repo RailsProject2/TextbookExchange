2879f14a72bbfdca900c0c86d44baa14a0233394
allanwlevy <allanwlevy@berkeley.edu>
2016-04-23 16:10:31 -0700

    buy and sell posts are now deletable

--- app/controllers/buy_posts_controller.rb
+++ app/controllers/buy_posts_controller.rb
@@ -12,7 +12,12 @@ class BuyPostsController < ApplicationController
 		@buy_post = BuyPost.new post_params
 		@buy_post.user = current_user
 		@buy_post.save
-		redirect_to action: 'index'
+		redirect_to users_show_path
+	end
+
+	def destroy
+		BuyPost.find(params[:id]).destroy
+		redirect_to users_show_path
 	end
 
 	def sort
